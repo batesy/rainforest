b42e2c87cba64250377626316ff67b803168f922
batesy <gbatesy@gmail.com>
2013-11-11 13:32:38 -0500

    Creating log in functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rainforest::Application.routes.draw do
+  get "sessions/new"
+  get "sessions/create"
+  get "sessions/destroy"
   get "users/new"
   get "users/create"
   resources :products
   root :to => 'products#index'
 
   resources :users, :only => [:new, :create]
+
+  resources :sessions, :only => [:new, :create, :destroy]
 end
